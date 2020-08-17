718f970741a7b5057116e61e6f79306af57305f0
Boczolaf <33521250+Boczolaf@users.noreply.github.com>
2020-08-17 19:01:51 +0200

    Fixed bug with keyboard movement

--- keyboardMovement.js
+++ keyboardMovement.js
@@ -3,17 +3,38 @@ function leftArrowPressed() {
         let index = currentlyChosenCell.cellIndex-1;
         let row = currentlyChosenCell.parentElement;
         if(index !== 0){
-            setCurrentlyChosenCell(row.cells[index]);
+            let cell = row.cells[index];
+            let type = cell.id.split("/")[1];
+            if(type.includes("cell")){
+                onClick(cell);
+            }
+            else if(type.includes("connection")){
+                onClickConnection(cell);
+            }
+            else if(type.includes("column")){
+                onClickForFirstRow(cell);
+            }
         }
     }
 }
 
 function rightArrowPressed() {
     if(typeof currentlyChosenCell !== 'string'){
+
         let index = currentlyChosenCell.cellIndex+1;
         let row = currentlyChosenCell.parentElement;
         if(index !== row.cells.length){
-            setCurrentlyChosenCell(row.cells[index]);
+            let cell = row.cells[index];
+            let type = cell.id.split("/")[1];
+            if(type.includes("cell")){
+                onClick(cell);
+            }
+            else if(type.includes("connection")){
+                onClickConnection(cell);
+            }
+            else if(type.includes("column")){
+                onClickForFirstRow(cell);
+            }
         }
     }
 }
@@ -24,20 +45,35 @@ function upArrowPressed() {
         let rowIndex = currentlyChosenCell.parentElement.rowIndex-1;
         let table = currentlyChosenCell.parentElement.parentElement;
         if(rowIndex !== 0){
-            setCurrentlyChosenCell(table.rows[rowIndex].cells[index]);
-        }
-
+            let cell = table.rows[rowIndex].cells[index];
+            changeSelectedCellForArrowMovement(cell);
     }
+}
 }
 
 function downArrowPressed() {
     if(typeof currentlyChosenCell !== 'string'){
+
         let index = currentlyChosenCell.cellIndex;
         let rowIndex = currentlyChosenCell.parentElement.rowIndex+1;
         let table = currentlyChosenCell.parentElement.parentElement;
         if(rowIndex !== table.rows.length){
-            setCurrentlyChosenCell(table.rows[rowIndex].cells[index]);
+            let cell = table.rows[rowIndex].cells[index];
+            changeSelectedCellForArrowMovement(cell);
         }
+    }
+}
+
+function changeSelectedCellForArrowMovement(cell){
+    let type = cell.id.split("/")[1];
+    if(type.includes("cell")){
+        onClick(cell);
+    }
+    else if(type.includes("connection")){
+        onClickConnection(cell);
+    }
+    else if(type.includes("column")){
+        onClickForFirstRow(cell);
     }
 }
 
